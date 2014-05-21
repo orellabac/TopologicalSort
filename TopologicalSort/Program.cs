b152064a8edb1eebd6789b5edeb8379d00a93418
orellabac <orellabac@gmail.com>
2014-05-21 09:47:29 -0600

    Uploading initial implementation

---
+++ TopologicalSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using QuickGraph;
+using QuickGraph.Algorithms;
+using System;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+	class Program
+	{
+
+		public static void ParseFile(AdjacencyGraph<String, SEdge<String>> graph, string inputFile)
+		{
+			var reader = new StreamReader(File.OpenRead(inputFile));
+			string line;
+			while ((line=reader.ReadLine())!=null)
+			{
+				line = line.Trim();
+				if (!string.IsNullOrWhiteSpace(line))
+				{
+					var nodes = line.Split(new char[] {'>',' '}, StringSplitOptions.RemoveEmptyEntries);
+					var firstNode = nodes[0];
+					var secondNode = nodes[1];
+					graph.AddVertex(firstNode);
+					graph.AddVertex(secondNode);
+					graph.AddEdge(new SEdge<string>(firstNode, secondNode));
+				}
+			}
+
+		}
+
+		static void Main(string[] args)
+		{
+			var graph = new AdjacencyGraph<String, SEdge<String>>();
+			ParseFile(graph, args[0]);
+			foreach (var element in graph.TopologicalSort())
+			{
+				Console.WriteLine(element);
+			}
+		}
+	}
+}
---
+++ TopologicalSort/readme.md
@@ -0,0 +1,20 @@
+﻿Topological Sort
+================
+
+Topological sort is a very useful algorithm when it comes to detect and analyse dependencies.
+In most modern languages this is automatically handled for you in the compilation process, but
+languages like `JavaScript` require you to solve this problem for them.
+
+Using the QuickGraph library I provide a very simple command line program that can be used to quickly solve this problem.
+
+Usage:
+=====
+
+Input file: use an input file with an format like the following:
+
+    file1   > file2
+    file3   > file4
+    
+Where `file1 > file2` means that file1 depends from file2.
+
+The output of the program will be all items sorted by dependency.
